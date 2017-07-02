766c74ee714037ea1d80c50d35a899ab966d45dc
Animesh Kashyap <annimesh2809@gmail.com>
2017-07-03 02:30:30 +0530

    Replace bfs! function with a union find ds

--- src/ImageSegmentation.jl
+++ src/ImageSegmentation.jl
@@ -4,25 +4,31 @@ module ImageSegmentation
 
 using Images, DataStructures, StaticArrays
 
+# For efficient hashing of CartesianIndex
+if !isdefined(Base.IteratorsMD, :cartindexhash_seed)
+    const cartindexhash_seed = UInt == UInt64 ? 0xd60ca92f8284b8b0 : 0xf2ea7c2e
+    function Base.hash(ci::CartesianIndex, h::UInt)
+        h += cartindexhash_seed
+        for i in ci.I
+            h = hash(i, h)
+        end
+        return h
+    end
+end
+
 include("core.jl")
 include("region_growing.jl")
-<<<<<<< 0d8720a6e224164e69d14c48dcdf4a042d358dda
 include("felzenszwalb.jl")
-=======
 include("fast_scanning.jl")
->>>>>>> Added fast scanning algorithm
 
 export
     # methods
     seeded_region_growing,
     unseeded_region_growing,
-<<<<<<< 0d8720a6e224164e69d14c48dcdf4a042d358dda
     felzenszwalb,
-    
-=======
-    fast_scanning
+    fast_scanning,
+    Nfast_scanning,
 
->>>>>>> Added fast scanning algorithm
     # types
     SegmentedImage,
     ImageEdge
--- src/fast_scanning.jl
+++ src/fast_scanning.jl
@@ -2,59 +2,31 @@
 # for 2-D images
 function fast_scanning{CT<:Colorant}(img::AbstractArray{CT,2}, threshold::Real, diff_fn::Function = default_diff_fn)
 
-    # Bfs coloring function
-    function bfs_color!(result::AbstractArray{Int, 2}, start_point::CartesianIndex{2}, label::Int)
-
-        prev_label = result[start_point]
-        bfs_queue  = Vector{CartesianIndex{2}}()
-        visited    = Set{CartesianIndex{2}}()
-
-        push!(visited, start_point)
-        push!(bfs_queue, start_point)
-
-        while !isempty(bfs_queue)
-            point = shift!(bfs_queue)
-            result[point] = label
-            for i in (CartesianIndex(point[1]-1,point[2]), CartesianIndex(point[1],point[2]-1), CartesianIndex(point[1]+1,point[2]), CartesianIndex(point[1],point[2]+1))
-                if checkbounds(Bool, result, i) && result[i] == prev_label
-                    if i ∉ visited
-                        push!(visited, i)
-                        push!(bfs_queue, i)
-                    end
-                end
-            end
-        end
-        result
-    end
-
     # Required data structures
     result              =   similar(dims->fill(-1,dims), indices(img))      # Array to store labels
     region_means        =   Dict{Int, Images.accum(CT)}()                   # A map conatining (label, mean) pairs
     region_pix_count    =   Dict{Int, Int}()                                # A map conatining (label, count) pairs
-    labels              =   Vector{Int}()                                   # Vector containing list of labels
-    removed_labels      =   Vector{Int}()                                   # Vector containing list of labels to be removed
+    temp_labels         =   IntDisjointSets(0)                              # Disjoint set to store map labels with their equivalence class
 
     for point in CartesianRange(indices(img))
-
         top_point = CartesianIndex(point[1]-1, point[2])
         left_point = CartesianIndex(point[1], point[2]-1)
         top_label = -1
         left_label = -1
 
-        if checkbounds(Bool, img, top_point) && diff_fn(region_means[result[top_point]], img[point]) < threshold
-            top_label = result[top_point]
+        if checkbounds(Bool, img, top_point) && diff_fn(region_means[find_root(temp_labels, result[top_point])], img[point]) < threshold
+            top_label = find_root(temp_labels, result[top_point])
         end
-        if checkbounds(Bool, img, left_point) && diff_fn(region_means[result[left_point]], img[point]) < threshold
-            left_label = result[left_point]
+        if checkbounds(Bool, img, left_point) && diff_fn(region_means[find_root(temp_labels, result[left_point])], img[point]) < threshold
+            left_label = find_root(temp_labels, result[left_point])
         end
 
         # If no valid point found then create a new label
         if top_label == -1 && left_label == -1
-            new_label = length(labels) + 1
+            new_label = push!(temp_labels)
             result[point] = new_label
             region_means[new_label] = img[point]
             region_pix_count[new_label] = 1
-            push!(labels, new_label)
 
         # Else if one valid label found, assign that label
         elseif top_label == -1 || left_label == -1
@@ -73,163 +45,108 @@ function fast_scanning{CT<:Colorant}(img::AbstractArray{CT,2}, threshold::Real, 
 
             # Else replace label having less pixel count with that having larger one and assign the label having larger pixel count
             else
-                min_count_point = region_pix_count[top_label] < region_pix_count[left_label] ? top_point: left_point
-                min_count_label = result[min_count_point]
-                max_count_label = min_count_label == top_label ? left_label : top_label
+                new_root = union!(temp_labels, top_label, left_label)
+                result[point] = new_root
 
-                result[point] = max_count_label
-                bfs_color!(result, min_count_point, max_count_label)
+                delete_label = top_label == new_root ? left_label : top_label
+                update_label = result[point]
 
-                region_pix_count[min_count_label] += 1
-                region_pix_count[max_count_label] += region_pix_count[min_count_label]
-                region_means[min_count_label] += (img[point] - region_means[min_count_label])/(region_pix_count[min_count_label])
-                region_means[max_count_label] += (region_means[min_count_label] - region_means[max_count_label])*region_pix_count[min_count_label]/region_pix_count[max_count_label]
+                region_pix_count[update_label] += region_pix_count[delete_label]
+                region_means[update_label] += (region_means[delete_label] - region_means[update_label])*region_pix_count[delete_label]/region_pix_count[update_label]
+                region_pix_count[update_label] += 1
+                region_means[update_label] += (img[point] - region_means[update_label])/(region_pix_count[update_label])
 
-                # Remove label result[i] from region_means, region_pix_count
-                delete!(region_pix_count, min_count_label)
-                delete!(region_means, min_count_label)
-                push!(removed_labels, min_count_label)
+                delete!(region_pix_count, delete_label)
+                delete!(region_means, delete_label)
             end
         end
     end
 
-    # Remove labels that have been recolored due to the bfs calls
-    @noinline sort_fn!(removed_labels) = sort!(removed_labels)
-    sort_fn!(removed_labels)
-    deleteat!(labels, removed_labels)
+    for point in CartesianRange(indices(img))
+        result[point] = find_root(temp_labels, result[point])
+    end
 
-    SegmentedImage(result, labels, region_means, region_pix_count)
+    SegmentedImage(result, unique(temp_labels.parents), region_means, region_pix_count)
 
 end
 
 
-# For N-D images
-
-function fast_scanning{CT<:Colorant,N}(img::AbstractArray{CT,N}, threshold::Real, diff_fn::Function = default_diff_fn)
+# for N-D images
+function Nfast_scanning{CT<:Colorant,N}(img::AbstractArray{CT,N}, threshold::Real, diff_fn::Function = default_diff_fn)
 
     # Neighbourhood functions
     _diagmN = diagm([1 for i in 1:N])
     half_region = ntuple(i-> CartesianIndex{N}(ntuple(j->_diagmN[j,i], Val{N})), Val{N})
-    neg_neighbourhood{N}(x::CartesianIndex{N}) = ntuple(i-> x-half_region[i], Val{N})
-    pos_neighbourhood{N}(x::CartesianIndex{N}) = ntuple(i-> x+half_region[i], Val{N})
-
-    # Bfs coloring function
-    function bfs_color!{N}(result::AbstractArray{Int, N}, start_point::CartesianIndex{N}, label::Int)
-
-        local prev_label = result[start_point]
-        local bfs_queue = Vector{CartesianIndex{N}}()
-        local visited = Set{CartesianIndex{N}}()
-
-        push!(visited, start_point)
-        push!(bfs_queue, start_point)
-
-        while !isempty(bfs_queue)
-            point = shift!(bfs_queue)
-            result[point] = label
-            for i in neg_neighbourhood(point)
-                if checkbounds(Bool, result, i) && result[i] == prev_label
-                    if i ∉ visited
-                        push!(visited, i)
-                        push!(bfs_queue, i)
-                    end
-                end
-            end
-            for i in pos_neighbourhood(point)
-                if checkbounds(Bool, result, i) && result[i] == prev_label
-                    if i ∉ visited
-                        push!(visited, i)
-                        push!(bfs_queue, i)
-                    end
-                end
-            end
-        end
-
-        result
-
-    end
+    n_gen(region) = x -> ntuple(i-> x-region[i], Val{N})
+    neighbourhood = n_gen(half_region)
 
     # Required data structures
     result              =   similar(dims->fill(-1,dims), indices(img))      # Array to store labels
     region_means        =   Dict{Int, Images.accum(CT)}()                   # A map conatining (label, mean) pairs
     region_pix_count    =   Dict{Int, Int}()                                # A map conatining (label, count) pairs
-    labels              =   Vector{Int}()                                   # Vector containing list of labels
-    removed_labels      =   Vector{Int}()                                   # Vector containing list of labels to be removed
-
+    temp_labels         =   IntDisjointSets(0)                              # Disjoint set to store map labels with their equivalence class
+    v                   =   MVector{N,Int}()                                # MVector to store valid neighbours
     for point in CartesianRange(indices(img))
-        v = Vector{CartesianIndex{N}}()
+        sz = 0
         same_label = true
         prev_label = 0
-        for p in neg_neighbourhood(point)
-            if checkbounds(Bool, img, p) && result[p] > 0
-                if diff_fn(region_means[result[p]], img[point]) < threshold
+        for p in neighbourhood(point)
+            if checkbounds(Bool, img, p)
+                root_p = find_root(temp_labels, result[p])
+                if diff_fn(region_means[root_p], img[point]) < threshold
                     if prev_label == 0
-                        prev_label = result[p]
-                    elseif prev_label != result[p]
+                        prev_label = root_p
+                    elseif prev_label != root_p
                         same_label = false
                     end
-                    push!(v, p)
+                    sz += 1
+                    v[sz] = find_root(temp_labels, root_p)
                 end
             end
         end
 
         # If no valid label found
-        if length(v) == 0
+        if sz == 0
             # Assign a new label
-            new_label = length(labels) + 1
+            new_label = push!(temp_labels)
             result[point] = new_label
             region_means[new_label] = img[point]
             region_pix_count[new_label] = 1
-            push!(labels, new_label)
 
         # If all labels are same
         elseif same_label
+
             result[point] = prev_label
             region_pix_count[prev_label] += 1
             region_means[prev_label] += (img[point] - region_means[prev_label])/(region_pix_count[prev_label])
         else
             # Merge segments and assign to this new label
-            # get max pixel_count segment
-            d = Dict{Int, Float64}()
-            for i in v
-                cur_label = result[i]
-                if ! haskey(d, cur_label)
-                    d[cur_label] = region_pix_count[cur_label]
-                end
-            end
-            max_count = -Inf
-            max_count_label = -1
-            for k in d
-                if k[2] > max_count
-                    max_count = k[2]
-                    max_count_label = k[1]
-                end
+            union_label = v[1]
+            for i in 1:sz
+                union_label = union!(temp_labels, union_label, v[i])
             end
 
-            result[point] = max_count_label
-            region_pix_count[max_count_label] += 1
-            region_means[max_count_label] += (img[point] - region_means[max_count_label])/(region_pix_count[max_count_label])
+            result[point] = union_label
+            region_pix_count[union_label] += 1
+            region_means[union_label] += (img[point] - region_means[union_label])/(region_pix_count[union_label])
 
             # Apply bfs on all points in v to make their label `max_count_label`
-            for i in v
-                if result[i] != max_count_label
-                    delete_label = result[i]
-                    bfs_color!(result, i, max_count_label)
-
-                    region_pix_count[max_count_label] += region_pix_count[result[i]]
-                    region_means[max_count_label] += (region_means[result[i]] - region_means[max_count_label])*region_pix_count[result[i]]/region_pix_count[max_count_label]
+            for i in 1:sz
+                if v[i] != union_label
+                    region_pix_count[union_label] += region_pix_count[v[i]]
+                    region_means[union_label] += (region_means[v[i]] - region_means[union_label])*region_pix_count[v[i]]/region_pix_count[union_label]
 
                     # Remove label result[i] from region_means, region_pix_count
-                    delete!(region_pix_count,delete_label)
-                    delete!(region_means,delete_label)
-                    push!(removed_labels,delete_label)
+                    delete!(region_pix_count,v[i])
+                    delete!(region_means,v[i])
                 end
             end
         end
     end
 
-    @noinline sort_fn!(removed_labels) = sort!(removed_labels)
-    sort_fn!(removed_labels)
-    deleteat!(labels, removed_labels)
+    for point in CartesianRange(indices(img))
+        result[point] = find_root(temp_labels, result[point])
+    end
 
-    SegmentedImage(result, labels, region_means, region_pix_count)
+    SegmentedImage(result, unique(temp_labels.parents), region_means, region_pix_count)
 end
