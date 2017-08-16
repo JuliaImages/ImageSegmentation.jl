7d6649d46ca606f1b642818cb50d40fe5cc550a6
Animesh Kashyap <annimesh2809@gmail.com>
2017-08-16 22:48:12 +0530

    Improved performance

--- src/fuzzy_cmeans.jl
+++ src/fuzzy_cmeans.jl
@@ -1,61 +1,66 @@
-function update_weights!(weights, data, centers, fuzziness, norm_fn)
-  pow = 2.0/(fuzziness-1)
-  ly, lx = length.(indices(weights))
-  for i in 1:ly
-    vali = data[:,i]
-    for j in 1:lx
-      den = 0.0
-      for k in 1:lx
-        den += (norm_fn(vali-centers[:,j])/norm_fn(vali-centers[:,k]))^pow
-      end
-      weights[i,j] = 1.0/den
+function update_weights!(weights, data, centers, fuzziness, dist_metric)
+    pow = 2.0/(fuzziness-1)
+    nrows, ncols = size(weights)
+    dists = pairwise(dist_metric, data, centers)
+    for i in 1:nrows
+        for j in 1:ncols
+            den = 0.0
+            for k in 1:ncols
+                den += (dists[i,j]/dists[i,k])^pow
+            end
+            weights[i,j] = 1.0/den
+        end
     end
-  end
 end
 
 function update_centers!(centers, data, weights, fuzziness)
-  ly, lx = length.(indices(weights))
-  for j in 1:lx
-    num = zero(data[:,1])
-    den = 0.0
-    for i in 1:ly
-      δm = weights[i,j]^fuzziness
-      num += δm * data[:,i]
-      den += δm
+    nrows, ncols = size(weights)
+    for j in 1:ncols
+        num = zeros(Float64, size(data[:,1]))
+        den = 0.0
+        for i in 1:nrows
+            δm = weights[i,j]^fuzziness
+            num += δm * data[:,i]
+            den += δm
+        end
+        centers[:,j] = num/den
     end
-    centers[:,j] = num/den
-  end
 end
 
-function fuzzy_cmeans(data::AbstractArray{T,2}, C::Int, totiter::Int, eps_::Real, fuzziness::Real, norm_fn::Function = norm) where T<:Real
-  ly, lx = length.(indices(data))
+function fuzzy_cmeans(data::AbstractArray{T,2}, C::Int, fuzziness::Real, totiter::Int = 100, eps_::Real = 1e-3, dist_metric::Metric = Euclidean(); debug::Bool = false) where T<:Real
+    nrows, ncols = size(data)
 
-  weights = rand(Float64, (lx, C))
-  for i in 1:lx
-    s = sum(weights[i,:])
-    weights[i,:] /= s
-  end
+    # Initialize weights randomly
+    weights = rand(Float64, ncols, C)
+    weights ./= sum(weights, 2)
 
-  centers = fill(0.0, ly, C)
+    centers = fill(0.0, nrows, C)
 
-  δ = Inf
-  iter = 0
-  prev_centers = identity.(centers)
+    δ = Inf
+    iter = 0
+    prev_centers = identity.(centers)
 
-  while iter < totiter && δ > eps_
-    update_centers!(centers, data, weights, fuzziness)
-    update_weights!(weights, data, centers, fuzziness, norm_fn)
-    δ = maximum([norm_fn(prev_centers[:,i] - centers[:,i]) for i in 1:C])
-    copy!(prev_centers, centers)
-    iter += 1
-    println(iter,": ",δ)
-  end
+    if debug
+        prog = ProgressThresh(eps_, "Converging:")
+    end
 
-  weights, centers
+    while iter < totiter && δ > eps_
+        update_centers!(centers, data, weights, fuzziness)
+        update_weights!(weights, data, centers, fuzziness, dist_metric)
+        δ = maximum(colwise(dist_metric, prev_centers, centers))
+        copy!(prev_centers, centers)
+        iter += 1
+        if debug
+            ProgressMeter.update!(prog, δ, showvalues = [(:Iteration, iter)])
+        end
+    end
+
+    weights, centers
 end
 
-function fuzzy_cmeans(img::AbstractArray{T,N}, rest...) where T<:Colorant where N
-  ch = channelview(img)
-  data = reshape(ch, :, *(length.(indices(img))...))
-  fuzzy_cmeans(data, rest...)
+function fuzzy_cmeans(img::AbstractArray{T,N}, rest...; debug::Bool = false) where T<:Colorant where N
+    pimg = parent(img)
+    ch = channelview(pimg)
+    data = reshape(ch, :, *(size(pimg)...))
+    fuzzy_cmeans(data, rest...; debug = debug)
 end
